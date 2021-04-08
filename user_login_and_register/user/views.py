3fd19403c50810ed664a61adbc5761d6f0b0de7c
Umar <usaleem651@gmail.com>
2021-04-08 15:02:32 +0500

    Login Django Forms

---
+++ user_login_and_register/user/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserRegisterForm
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.template import Context
+
+
+def index(request):
+    return render(request, 'user/index.html',{'title':'Welcome Page'})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST) or None
+        if form.is_valid():
+            username = request.POST.get('username')
+            htmly = get_template('user/Email.html')
+            d = { 'username': username }
+            subject, from_email, to = 'Confirmaion Mail', 'usaleem651@gmail.com', 'usaleem651@gmail.com'
+            html_content = htmly.render(d)
+            msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            try:
+                msg.send()
+            except:
+                print("error in sending mail")
+            ##################################################################
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to Log In')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'user/register.html', {'form': form,'title':'REGISTER'})
+
+
+def Login(request):
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            form = login(request,user)
+            messages.success(request, f' Welcome {username} !!')
+            return redirect('index')
+        else:
+            messages.info(request, f'Account does not exist please Sign In')
+    form = AuthenticationForm()
+    return render(request, 'user/login.html', {'form':form,'title':'LOGIN'})
